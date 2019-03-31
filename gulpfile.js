dd08b9a375eab6726352b531ff4650a0601edd5d
kouichi hoshi <kouichi.hoshi@stella-design.biz>
2019-03-31 09:23:08 +0900

    gulpfile.js,paceage.json update

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,72 @@
-var fs           = require('fs');
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var plumber      = require('gulp-plumber');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps   = require('gulp-sourcemaps');
-var notify       = require('gulp-notify');
-var watch        = require('gulp-watch');
-var htmlhint     = require('gulp-htmlhint');
+//const fs           = require('fs');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const plumber      = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps   = require('gulp-sourcemaps');
+const notify       = require('gulp-notify');
+const htmlhint     = require('gulp-htmlhint');
 
 // ディレクトリ設定
-var dir = {
+const dir = {
 	src: '_src',
-	dist: 'dist'//WordPressのテーマ格納ディリクトリを指定
+	dist: 'dist'
 };
 
 //copyタスクのソース
-var copySource = [
-	dir.src + '/**/',
-	'!' + dir.src + '/_*/',
-	'!' + dir.src + '/_*/**/',
+const copySource = [
+	dir.src + '/**/**.html',
+	dir.src + '/**/vendor/**',
 ];
 
 //sassファイル監視対象指定
-var watchSass = [
+const watchSass = [
 	dir.src + '/_sass/**/*.scss',
-]
+];
 
 // デスクトップにエラー通知を出す
-var errorOpt = {
+const errorOpt = {
 	errorHandler: notify.onError('Error: [エラー発生!] <%= error %>')
 };
 
 //指定したディリクトリのsassファイルを指定したディリクトリにコンパイルする
-gulp.task('sass', function() {
-	gulp.src(watchSass)
-		.pipe(plumber(errorOpt))
-		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'expanded'}))
-		.pipe(autoprefixer("last 3 version"))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(dir.dist));
+gulp.task('sass', () => {
+	return (
+		gulp.src(watchSass)
+			.pipe(plumber(errorOpt))
+			.pipe(sourcemaps.init())
+			.pipe(sass({outputStyle: 'expanded'}))
+			.pipe(autoprefixer("last 3 version"))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest(dir.dist))
+	);
 });
 
-
 //指定したファイルをコピーする
-gulp.task('copy', function() {
-	gulp.src(copySource,{base: dir.src}) //,{base: dir.src}と記述するとコピー元のディリクトリ構造を維持する
-   .pipe(gulp.dest( dir.dist ));
+gulp.task('copy', () => {
+	return (
+		gulp.src(copySource,{base: dir.src}) //,{base: dir.src}と記述するとコピー元のディリクトリ構造を維持する
+	   .pipe(gulp.dest( dir.dist ))
+	);
 });
 
 //html構文チェック
-gulp.task('htmlhint', function(){
-	gulp.src(dir.dist + '/**/*.html')
-	.pipe(htmlhint())
-	.pipe(htmlhint.reporter());
+gulp.task('htmlhint', () => {
+	return (
+		gulp.src(dir.dist + '/**/*.html')
+		.pipe(htmlhint())
+		.pipe(htmlhint.reporter())
+	);
 });
 
-//指定したディリクトリ、ファイルを監視してタスクを実行する
-gulp.task('watch', function () {
-	watch(watchSass, function(){
-		gulp.start('sass');
-	});
-	watch(copySource, function(){
-		gulp.start('copy');
-	});
-	// watch(dir.dist + '/**/*.html', function(){
-	// 	gulp.start('htmlhint');
-	// });
+// watch sassコンパイル実行、指定ファイルをdistにコピー実行、htmlhint実行
+gulp.task("watch", () => {
+	gulp.watch(watchSass, gulp.task('sass'));
+	gulp.watch(dir.dist + '/**/*.html', gulp.task('htmlhint'));
+	gulp.watch(copySource, gulp.task('copy'));
 });
 
-// デフォルトのgulpコマンドで実行されるタスクを指定
-gulp.task('default', ['sass', 'copy', 'htmlhint']);
-
-
+//  default（build） sassコンパイル、指定ファイルをdistにコピー、htmlhint 各一回実行
+gulp.task('default',gulp.series('sass', 'htmlhint', 'copy', (done) => {
+	done();
+}));
